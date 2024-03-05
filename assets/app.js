238fafac4a24dcfdf0f5892428767038f2ce0cd8
ajay-bishnoi <AJAY2003BISHNOI@GMAIL.COM>
2024-03-05 11:02:03 +0530

    auto play slider

--- assets/app.js
+++ assets/app.js
@@ -28,7 +28,7 @@ $(".mySlider").slick({
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false,
+        autoplay: true,
         autoplaySpeed: 2200,
         infinite: true,
         dots: false,
